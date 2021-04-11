8f396648408ea4ce19219420bc76259a35ef32b1
リリ <riri@ririnoMacBook-Pro.local>
2021-04-11 15:48:22 +0900

    ストロングパラメーターの記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  
   before_action :authenticate_user!
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -6,7 +8,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :encrypted_passwor, :nickname, :last_name, :family_name, :j_last_name, :j_family_name, :birthday ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :nickname, :email, :encrypted_password, :family_name, :last_name, :j_family_name, :j_last_name, :birthday ])
   end
   
   def basic_auth
