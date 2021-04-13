f78cb5c5a165b801d23e4b4e6d31005121db3296
リリ <riri@ririnoMacBook-Pro.local>
2021-04-13 15:18:03 +0900

    rubocop適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,29 +2,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #パスワードのバリデーション
-  with_options confirmation: { message: "%{encrypted_password}と%{password_confirmation}は一致しないといけません" },
-               format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: "は半角英数字混合での入力が必須です。" } do
+  # パスワードのバリデーション
+  with_options confirmation: { message: '%{encrypted_password}と%{password_confirmation}は一致しないといけません' },
+               format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: 'は半角英数字混合での入力が必須です。' } do
     validates :encrypted_password
     validates :password_confirmation, presence: true
   end
 
-  #ニックネームのバリデーション
+  # ニックネームのバリデーション
   validates :nickname, presence: true
 
-  #苗字、氏名のバリデーション
-  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥]+\z/, message: "は、全角（漢字・ひらがな・カタカナ）での入力が必須です。" } do
+  # 苗字、氏名のバリデーション
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥]+\z/, message: 'は、全角（漢字・ひらがな・カタカナ）での入力が必須です。' } do
     validates :last_name
     validates :family_name
   end
 
-  #苗字、氏名のかな入力のバリデーション
-  with_options presence: true, format: { with: /\A[ァ-ヶーー]+\z/, message: "は、全角（カタカナ）での入力が必須です。" } do
-    validates :j_last_name 
+  # 苗字、氏名のかな入力のバリデーション
+  with_options presence: true, format: { with: /\A[ァ-ヶーー]+\z/, message: 'は、全角（カタカナ）での入力が必須です。' } do
+    validates :j_last_name
     validates :j_family_name
   end
 
-  #生年月日のバリデーション
+  # 生年月日のバリデーション
   validates :birthday, presence: true
-
 end
