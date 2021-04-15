014a45d896e10d8f861e26cbd70062de86629df4
リリ <riri@ririnoMacBook-Pro.local>
2021-04-15 17:15:31 +0900

    商品出品ページへのアクセス権限の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :require_login]
+  before_action :set_item, only: [:show, :edit, :update]
+
+
   def index
   end
 
@@ -6,5 +10,13 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :product_name, :explanation, :category_id, :status_id, :shipping_charges_id, :prefectures_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
+
+  def set_item
+    @items = Item.find(params[:id])
   end
 end
