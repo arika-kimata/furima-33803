425b48dc75c7b2c631e655a9e78e77f7da9b9945
リリ <riri@ririnoMacBook-Pro.local>
2021-04-15 22:14:43 +0900

    new,createアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,31 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :require_login]
+  before_action :authenticate_user!, except: [:index, :require_login]
   before_action :set_item, only: [:show, :edit, :update]
 
 
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :product_name, :explanation, :category_id, :status_id, :shipping_charges_id, :prefectures_id, :shipping_date_id, :price).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    redirect_to root_path unless user_signed_in?
   end
 
   def set_item
