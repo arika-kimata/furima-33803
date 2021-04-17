bb80a103f68e72b64c960c01fd1a4498c95567ab
リリ <riri@ririnoMacBook-Pro.local>
2021-04-17 17:43:16 +0900

    ダミー画像、ダミー商品の記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,9 +5,9 @@ FactoryBot.define do
     email                 { 'satotaro@test' }
     password              { 'sato1111' }
     password_confirmation { 'sato1111' }
-    family_name { '佐藤' }
-    last_name { '太郎' }
-    j_family_name { 'サトウ' }
+    family_name           { '佐藤' }
+    last_name             { '太郎' }
+    j_family_name         { 'サトウ' }
     j_last_name           { 'タロウ' }
     birthday              { '2000-01-01' }
   end
