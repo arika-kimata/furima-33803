f35abe088a0fc14a4e8072e305ab2ccfc4858cfc
リリ <riri@ririnoMacBook-Pro.local>
2021-05-27 10:16:22 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'messages/new'
   devise_for :users
 
   root to: 'items#index'
   
   resources :items do
     resources :orders, only: [:index, :create]
+    resources :messages, only: [:create]
   end
 
 end
