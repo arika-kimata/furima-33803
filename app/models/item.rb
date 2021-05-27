f82bd1677a642c9052d86b3d3f17c7ccb854e076
リリ <riri@ririnoMacBook-Pro.local>
2021-05-27 10:15:59 +0900

    messageモデルの作成、アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_one :order
+  has_many :messages #, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
